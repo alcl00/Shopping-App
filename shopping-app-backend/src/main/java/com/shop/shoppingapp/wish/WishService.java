30f0dc0ba79749657099980e06a2edc143d25bf5
alcl00 <61521851+alcl00@users.noreply.github.com>
2023-05-08 20:35:21 -0400

    updated deleteWish method to use a stream
    Removed Iterator and while loop, implemented a stream to find a specific wish to delete

--- shopping-app-backend/src/main/java/com/shop/shoppingapp/wish/WishService.java
+++ shopping-app-backend/src/main/java/com/shop/shoppingapp/wish/WishService.java
@@ -3,6 +3,7 @@ package com.shop.shoppingapp.wish;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,68 +15,67 @@ import com.shop.shoppingapp.product.ProductRepository;
 
 @Service
 public class WishService {
-	
+
 	private final WishRepository wishRepository;
 	private final ProductRepository productRepository;
 	private final CustomerRepository customerRepository;
-	
+
 	@Autowired
 	public WishService(WishRepository wishRepository, ProductRepository productRepository,
-			CustomerRepository customerRepository) {
+					   CustomerRepository customerRepository) {
 		this.wishRepository = wishRepository;
 		this.productRepository = productRepository;
 		this.customerRepository = customerRepository;
 	}
-	
+
 	public void addToWishlist(Long customerId, String UPC) {
 		Optional<Customer> customerMaybe = customerRepository.findById(customerId);
-		
+
 		if(customerMaybe.isEmpty()) {
 			throw new IllegalStateException("Customer with ID " + customerId + " does not exist");
 		}
-		
+
 		Optional<Product> productMaybe = productRepository.findById(UPC);
-		
+
 		if(productMaybe.isEmpty()) {
 			throw new IllegalStateException("Product with UPC :" + UPC + " not found");
 		}
-		
+
 		wishRepository.save(new Wish(customerMaybe.get(), productMaybe.get()));
 	}
-	
+
 	public void deleteFromWishlist(Long customerId, String UPC) {
-		
+
 		Optional<Customer> customerMaybe = customerRepository.findById(customerId);
-		
+
 		if(customerMaybe.isEmpty()) {
 			throw new IllegalStateException("Customer with ID " + customerId + " does not exist");
 		}
-		
+
 		Optional<Product> productMaybe = productRepository.findById(UPC);
-		
+
 		if(productMaybe.isEmpty()) {
 			throw new IllegalStateException("Product with UPC :" + UPC + " not found");
 		}
-		
-		Iterator<Wish> it = wishRepository.findByCustomerId(customerId).iterator();
-		
-		while(it.hasNext()) {
-			Wish w = it.next();
-			if(w.getCustomer().getId() == customerId && w.getProduct().getUPC() == UPC) {
-				wishRepository.deleteById(w.getId());
-				break;
-			}
-		}
+
+		Predicate<Wish> wishPredicate = wish -> wish.getCustomer().getId().equals(customerId) && wish.getProduct().getUPC().equals(UPC);
+
+		Optional<Wish> wishToDelete = wishRepository.findByCustomerId(customerId).stream()
+				.filter(wishPredicate)
+				.findFirst();
+
+		wishRepository.deleteById(wishToDelete.get().getId());
+
 	}
-	
+
 	public List<Wish> getCustomerWishlist(Long customerId) {
-		
+
 		Optional<Customer> customerMaybe = customerRepository.findById(customerId);
-		
+
 		if(customerMaybe.isEmpty()) {
 			throw new IllegalStateException("Customer with ID " + customerId + " does not exist");
 		}
-		
+
 		return wishRepository.findByCustomerId(customerId);
 	}
 }
